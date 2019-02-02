1b7367af5f9ea162cf86edd83acff32e2f6fe5b2
Lightman <yaruk.my@gmail.com>
2019-02-02 12:52:10 +0200

    init

---
+++ src/NamespaceHelper.php
@@ -0,0 +1,96 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\NamespaceHelper;
+
+class NamespaceHelper
+{
+    /**
+     * @var array
+     */
+    protected $source;
+
+    public function __construct(array $source)
+    {
+        $this->source = $source;
+    }
+
+    public static function fromSource(array $source)
+    {
+        return new static($source);
+    }
+
+    public static function from(array $source)
+    {
+        return static::fromSource($source);
+    }
+
+    public static function fromDotted(string $source)
+    {
+        return static::fromSource(explode('.', $source));
+    }
+
+    public function toDotted(): string
+    {
+        return implode('.', $this->source);
+    }
+
+    public function toDirectory(): string
+    {
+        return implode(DIRECTORY_SEPARATOR, $this->source);
+    }
+
+    public function toDashed(bool $toLower = true): string
+    {
+        $source = $this->source;
+        if ($toLower) {
+            $source = array_map('strtolower', $source);
+        }
+        return implode('-', $source);
+    }
+
+    public function toSlashed(bool $toLower = true): string
+    {
+        $source = $this->source;
+        if ($toLower) {
+            $source = array_map('strtolower', $source);
+        }
+        return implode('/', $source);
+    }
+
+    public function toNamespace(): string
+    {
+        return implode('\\', $this->source);
+    }
+
+    public function get()
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param $part
+     * @return $this
+     */
+    public function prepend($part)
+    {
+        $part = (array) $part;
+        $this->source = array_merge($part, $this->source);
+        return $this;
+    }
+
+    /**
+     * @param $part
+     * @return $this
+     */
+    public function append($part)
+    {
+        $part = (array) $part;
+        $this->source = array_merge($this->source, $part);
+        return $this;
+    }
+
+    public function clone()
+    {
+        return static::fromSource($this->source);
+    }
+}
---
+++ tests/NamespaceTest.php
@@ -0,0 +1,101 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\NamespaceHelper\Tests;
+
+use Mrself\NamespaceHelper\NamespaceHelper;
+use PHPUnit\Framework\TestCase;
+
+class NamespaceTest extends TestCase
+{
+    public function testFrom()
+    {
+        $ns = NamespaceHelper::from([1]);
+        $this->assertEquals([1], $ns->get());
+    }
+
+    public function testFromSource()
+    {
+        $ns = NamespaceHelper::fromSource([1]);
+        $this->assertEquals([1], $ns->get());
+    }
+
+    public function testFromDotted()
+    {
+        $ns = NamespaceHelper::fromDotted('a.b');
+        $this->assertEquals(['a', 'b'], $ns->get());
+    }
+
+    public function testToDotted()
+    {
+        $ns = NamespaceHelper::from(['a', 'b']);
+        $this->assertEquals('a.b', $ns->toDotted());
+    }
+
+    public function testToDirectory()
+    {
+        $ns = NamespaceHelper::from(['a', 'b']);
+        $this->assertEquals('a' . DIRECTORY_SEPARATOR . 'b', $ns->toDirectory());
+    }
+
+    public function testToLowerDashed()
+    {
+        $ns = NamespaceHelper::from(['a', 'B']);
+        $this->assertEquals('a-b', $ns->toDashed());
+    }
+
+    public function testToDashedTotLower()
+    {
+        $ns = NamespaceHelper::from(['a', 'B']);
+        $this->assertEquals('a-B', $ns->toDashed(false));
+    }
+
+    public function testToLowerSlashed()
+    {
+        $ns = NamespaceHelper::from(['a', 'B']);
+        $this->assertEquals('a/b', $ns->toSlashed());
+    }
+
+    public function testToLowerSlashedNotLower()
+    {
+        $ns = NamespaceHelper::from(['a', 'B']);
+        $this->assertEquals('a/B', $ns->toSlashed(false));
+    }
+
+    public function testGet()
+    {
+        $ns = NamespaceHelper::fromSource([1]);
+        $this->assertEquals([1], $ns->get());
+    }
+
+    public function testPrependString()
+    {
+        $ns = NamespaceHelper::from(['a']);
+        $this->assertEquals(['b', 'a'], $ns->prepend('b')->get());
+    }
+
+    public function testPrependArray()
+    {
+        $ns = NamespaceHelper::from(['a']);
+        $this->assertEquals(['b', 'a'], $ns->prepend(['b'])->get());
+    }
+
+    public function testAppendString()
+    {
+        $ns = NamespaceHelper::from(['a']);
+        $this->assertEquals(['a', 'b'], $ns->append('b')->get());
+    }
+
+    public function testAppendArray()
+    {
+        $ns = NamespaceHelper::from(['a']);
+        $this->assertEquals(['a', 'b'], $ns->append(['b'])->get());
+    }
+
+    public function testClone()
+    {
+        $ns1 = NamespaceHelper::from(['a']);
+        $ns2 = $ns1->clone();
+        $ns1->append('b');
+        $this->assertEquals(['a'], $ns2->get());
+    }
+}
